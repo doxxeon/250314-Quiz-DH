ffa41e5c828addc42a612303b60db42c4d5af17c
Dohyeon <dohyun897@naver.com>
2025-03-14 10:59:15 +0900

    수정

--- q10.py
+++ q10.py
@@ -7,7 +7,7 @@ df = sns.load_dataset('tips')
 
 # countplot함수의 출력물을 sns_plot_size으로 저장
 # x축을 "size" 컬럼으로 하여 "size"에 대한 countplot을 그림
-sns_plot_size = sns.countplot(x='size', data=df)
+sns_plot_size = sns.countplot(x='size', data=df, palette='tab10')
 
 # jointplot함수의 출력물을 g로 저장
 # x축은 "total_bill", y축은 "tip", 차트의 종류는 "resid"으로 하여 jointplot을 그림
